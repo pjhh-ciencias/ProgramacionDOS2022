fb37cf1f94065d8e8e82f8b34d6908c1a080145e
pjhh-ciencias <pjhh@ciencias.unam.mx>
2023-03-25 16:14:29 -0600

    Added

---
+++ Programas/Poligono.py
@@ -0,0 +1,28 @@
+import turtle
+
+n=int(input('Ingresa un número entero mayor que 2: '))
+
+while n <=2:
+    print('El número debe ser entero mayor que 2.')
+    n=int(input('Ingresa un número entero mayor que 2: '))
+
+longitud=50
+giro=360//n
+
+turtle.shape("turtle")
+turtle.color("black","blue")
+turtle.speed("normal")
+
+for j in range(18):
+    turtle.left(20)
+    for i in range(n):
+        turtle.forward(longitud)
+        turtle.left(giro)
+
+turtle.hideturtle()
+turtle.exitonclick()
+
+
+
+
+
