95598e6b99ba6829efc6b2240898e48732d46c3b
PabloJorgeHdez <pablin9104_@hotmail.com>
2023-03-27 21:05:43 -0600

    Added

--- Programas/Poligono.py
+++ Programas/Poligono.py
@@ -6,14 +6,14 @@ while n <=2:
     print('El número debe ser entero mayor que 2.')
     n=int(input('Ingresa un número entero mayor que 2: '))
 
-longitud=100
+longitud=50
 giro=360/n
 
 turtle.shape("turtle")
 turtle.color("black","blue")
-turtle.speed("normal")
+turtle.speed("fastest")
 
-for j in range(60):
+for j in range(72):
     turtle.left(5)
     for i in range(n):
         turtle.forward(longitud)
